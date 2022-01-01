e5d81efd1e5b5c091176cd72db559e61c93b2ed5
amir-mln <devcareer.me@gmail.com>
2022-01-01 04:28:31 +0330

    parsing requests

--- section-5/index.js
+++ section-5/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("first middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">submit</submit></form>'
+  );
 });
 
-app.use((req, res) => {
-  console.log("second middleware");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.listen(3000, () => console.log("started the server on port 3000"));
+app.listen(3000);
