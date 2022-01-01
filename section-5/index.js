5d737bbefa3596bbc5bddf359640e8242104184a
amir-mln <devcareer.me@gmail.com>
2022-01-01 03:47:28 +0330

    working on `middlewares`

--- section-5/index.js
+++ section-5/index.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("first middleware");
+  next();
+});
+
+app.use((req, res) => {
+  console.log("second middleware");
+  res.send("<h1>Hello World!</h1>");
+});
+
 app.listen(3000, () => console.log("started the server on port 3000"));
