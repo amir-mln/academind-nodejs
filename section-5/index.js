328e34f2e99b2cdb9b870927b9cf37239ca25083
amir-mln <devcareer.me@gmail.com>
2022-01-13 13:26:34 +0330

    styling `views` and serving static files

--- section-5/index.js
+++ section-5/index.js
@@ -4,13 +4,17 @@ const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const rootDir = require("./utils/path");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(rootDir, "public")));
+
 app.use("/admin", adminRoutes);
 
-app.use("/shop", shopRoutes);
+app.use("/", shopRoutes);
 
 app.use((_, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
