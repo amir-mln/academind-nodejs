3cb0b4d946e85298fd0f415e8d6522691204abbf
amir-mln <devcareer.me@gmail.com>
2022-01-13 15:00:19 +0330

    added templating by `ejs`

--- section-5/index.js
+++ section-5/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 
-const adminRoutes = require("./routes/admin");
+const adminRoutes = require("./routes/admin").router;
 const shopRoutes = require("./routes/shop");
 const rootDir = require("./utils/path");
 
 const app = express();
+
+app.set("view engine", "ejs");
+
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
