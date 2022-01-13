50ef03141b3fbcb270ba73b0dd151b0055e6839f
amir-mln <devcareer.me@gmail.com>
2022-01-13 11:02:02 +0330

    sending files and using `path`

--- section-5/routes/admin.js
+++ section-5/routes/admin.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
-router.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">submit</submit></form>'
-  );
+router.get("/add-product", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
