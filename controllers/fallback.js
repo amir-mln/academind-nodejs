a6580c121b204413a152a63b3e202a5086ff13f4
amir-mln <devcareer.me@gmail.com>
2022-01-18 21:22:40 +0330

    finished `section-8`

--- controllers/fallback.js
+++ controllers/fallback.js
@@ -1,7 +1,5 @@
 const path = require("path");
 
-const rootDir = require("../utils/path");
-
 module.exports = (_, res) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
 };
--- models/product.js
+++ models/product.js
@@ -1,25 +1,40 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const rootDir = require("../utils/path");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const p = path.join(rootDir, "data", "products.json");
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    Product.fetchAll((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), console.log);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
     });
   }
 
-  static fetchAll(callback) {
-    fs.readFile(p, (err, fileContent) =>
-      callback(err ? [] : JSON.parse(fileContent))
-    );
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
